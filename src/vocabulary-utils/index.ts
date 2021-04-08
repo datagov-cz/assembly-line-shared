b12d8a424e119c9f11a8843e9e8ca0dab17c8590
Petr Křemen <petr.kremen@gmail.com>
2021-04-08 15:25:24 +0200

    [#3] fix prettier formatting

--- src/vocabulary-utils/index.ts
+++ src/vocabulary-utils/index.ts
@@ -5,15 +5,15 @@ import vocabularyTypes from "./vocabularyTypes.json";
  * `const shortLabel = getVocabularyShortLabel("https://slovník.gov.cz/agendový/101")`
  */
 export function getVocabularyShortLabel(vocabularyIri: string) {
-    const vocabularies = vocabularyTypes
-        .map((v) => {
-            const match = vocabularyIri.match(v.regex);
-            return !match ? null : (
-                (match.length == 1) ? v.shortName : v.shortName.replace("{id}", match[1])
-            );
-        })
-        .filter((v) => v);
-    return (!vocabularies || vocabularies.length > 1) ?
-        null :
-        vocabularies[0];
+  const vocabularies = vocabularyTypes
+    .map((v) => {
+      const match = vocabularyIri.match(v.regex);
+      return !match
+        ? null
+        : match.length == 1
+        ? v.shortName
+        : v.shortName.replace("{id}", match[1]);
+    })
+    .filter((v) => v);
+  return !vocabularies || vocabularies.length > 1 ? null : vocabularies[0];
 }
