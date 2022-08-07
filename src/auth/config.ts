883b391607a2f34e029e6a083ece48691a9bd351
Karel Klíma <karelklima@gmail.com>
2022-08-07 12:11:36 +0200

    Added base64 encoding for auth forward URI

--- src/auth/config.ts
+++ src/auth/config.ts
@@ -1,6 +1,21 @@
 import { getEnvInstance } from "env";
 
 /**
+ * Base64 encoding helper
+ */
+const encodeBase64 = (uri: string) => {
+  return Buffer.from(uri).toString("base64");
+};
+
+/**
+ * Forward URI encoding helper
+ */
+const encodeForwardUri = (uri: string) => {
+  // Since base64 produces equal signs on the end, it needs to be further encoded
+  return encodeURI(encodeBase64(uri));
+};
+
+/**
  * OIDC variables
  */
 export const getOidcConfig = () => {
@@ -12,7 +27,7 @@ export const getOidcConfig = () => {
   return {
     authority: COMPONENTS["al-auth-server"].url,
     client_id: ID,
-    redirect_uri: `${URL}/oidc-signin-callback.html?forward_uri=${encodeURI(
+    redirect_uri: `${URL}/oidc-signin-callback.html?forward_uri=${encodeForwardUri(
       URL
     )}`,
     silent_redirect_uri: `${URL}/oidc-silent-callback.html`,
@@ -29,9 +44,9 @@ export const getOidcConfig = () => {
  */
 export const generateRedirectUri = (forwardUri: string) => {
   const env = getEnvInstance();
-  return `${env.get("URL")}/oidc-signin-callback.html?forward_uri=${encodeURI(
-    forwardUri
-  )}`;
+  return `${env.get(
+    "URL"
+  )}/oidc-signin-callback.html?forward_uri=${encodeForwardUri(forwardUri)}`;
 };
 
 /**
